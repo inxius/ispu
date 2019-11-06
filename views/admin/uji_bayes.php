625226910f170e90b26dd4825d2d2bec96268036
jafar001 <jsodiq88@gmail.com>
2019-11-06 08:37:25 +0700

    tampilan hasil uji bayes

--- views/admin/uji_bayes.php
+++ views/admin/uji_bayes.php
@@ -1,25 +1,18 @@
 <?php
-  // session_start();
-  // if (!isset($_SESSION['dataFitur']) || !isset($_SESSION['dataUji'])) {
-  //   header("location:pengujian_bayes.php");
-  // }
-  // $dataFitur = json_encode($_SESSION['dataFitur']);
-  // $dataUji = json_encode($_SESSION['dataUji']);
-  // echo count($_SESSION['dataUji']);
-  // unset($_SESSION['dataFitur']);
-  // unset($_SESSION['dataUji']);
   $dataFiturEnc = json_encode($dataFitur);
   $dataUjiEnc = json_encode($dataUji);
-  echo "<pre>";
-  print_r($dataUji);
-  echo "</pre>";
+  // echo "<pre>";
+  // print_r($dataUji);
+  // echo "</pre>";
  ?>
 
  <!DOCTYPE html>
  <html lang="en" dir="ltr">
    <head>
      <meta charset="utf-8">
-     <title>UJI NAIVE BAYES</title>
+     <title>Nive Bayes - Pengujian Data Uji</title>
+     <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+     <link rel="stylesheet" href="../../assets/css/custom.css">
 
      <script type="text/javascript" src="../../Controller/C_math.js"></script>
      <script type="text/javascript">
@@ -34,6 +27,42 @@
      </script>
    </head>
    <body>
+     <nav class="navbar navbar-expand-sm fixed-top navbar-dark bg-dark">
+       <?php
+       include "navbar.php";
+       unset($dataUjiEnc);
+       unset($dataLatihEnc);
+       ?>
+     </nav>
 
+     <div class="jumbotron jumbotron-fluid bg-dark">
+       <div class="jumbotron-background">
+         <img src="../../assets/img/color1.jpg" class="blur ">
+       </div>
+
+       <div class="container content">
+         <div class="border-bottom d-flex justify-content-center align-items-center">
+           <h1 style="color:white;"><b>Hasil Pengujian Data Uji Dengan Metode Naive Bayes</b></h1>
+         </div>
+         <br>
+         <div class="row d-flex justify-content-center align-items-center">
+           <div class="col-md-6 col-sm-6 text-center">
+             <h1 style="color:white;"><b><script>document.write("Akurasi = "+akurasiBayes[0])</script> %</b></h1>
+           </div>
+         </div>
+         <div class="row d-flex justify-content-center align-items-center">
+           <div class="col-md-3 col-sm-3 text-left">
+             <ul>
+               <li>Total Data Uji = <script>document.write(akurasiBayes[1])</script></li>
+               <li>Benar = <script>document.write(akurasiBayes[2])</script></li>
+               <li>Salah = <script>document.write(akurasiBayes[3])</script></li>
+             </ul>
+             <br>
+             <button class="showButton show btn btn-primary btn-full" name="tampil">Tampilkan Perhitungan</button>
+             <button class="hideButton hidden btn btn-primary btn-full" name="hide">Sembunyikan Perhitungan</button>
+           </div>
+         </div>
+       </div>
+     </div>
    </body>
  </html>
